93ffd98b0cfa261183fdb56107582c8124f42cb4
youngcy <youngcy.youngcy@gmail.com>
2017-08-22 10:02:21 +0800

    update feature select

--- feature_selection.py
+++ feature_selection.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 
+from sklearn.preprocessing import Binarizer
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import VarianceThreshold
 from sklearn.feature_extraction.text import CountVectorizer
 
 from datasets import Datasets
@@ -34,6 +36,11 @@ def main():
     X_new = modle.transform(cv_datasets)
     print X_new.shape
 
+    binarizer = Binarizer(threshold=1.0)
+    b_datasets = binarizer.fit_transform(cv_datasets)
+    variance_threshold = VarianceThreshold(.8*(1-.8))
+    v_datasets = variance_threshold.fit_transform(b_datasets)
+    print v_datasets.shape
 
 if __name__ == '__main__':
     main()
